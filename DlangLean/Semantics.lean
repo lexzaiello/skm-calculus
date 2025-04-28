bffeede3d0003e45a13dd3659fbdc28f85697e6e
Dowland Aiello <dowlandaiello@gmail.com>
2025-04-28 16:18:45 -0700

    All compiling.

--- DlangLean/Semantics.lean
+++ DlangLean/Semantics.lean
@@ -26,40 +26,65 @@ open LExpr
 -- Note that reduction happens in inference, too
 -- as per 6. https://en.wikipedia.org/wiki/Calculus_of_constructions
 
-def eval_infer (e : LExpr) : StateT (List $ (ℕ × (LExpr × LExpr))) Option (LExpr × LExpr) :=
+def substitute (idx : ℕ) (with_expr : LExpr) : LExpr → LExpr
+  | abstraction x bind_ty body => abstraction x (substitute idx with_expr bind_ty) (substitute idx with_expr body)
+  | fall x bind_ty body => fall x (substitute idx with_expr bind_ty) (substitute idx with_expr body)
+  | t@(ty _) => t
+  | prp => prp
+  | v@(bvar n) => if n == idx then with_expr else v
+  | app lhs rhs =>
+    app (substitute idx with_expr lhs) (substitute idx with_expr rhs)
+
+-- Evaluate in some number of terms
+def eval (e : LExpr) (n_max_steps : ℕ) : Option LExpr := match n_max_steps with
+  | Nat.zero => none
+  | Nat.succ pred =>
+    match e with
+      | app lhs rhs =>
+        match lhs with
+        | abstraction idx _ body => substitute idx rhs body
+        | x => (app . rhs) <$> eval x pred
+      | x => pure x
+
+structure Context where
+  types : List $ ℕ × LExpr
+
+def infer (e : LExpr) : StateT Context Option LExpr :=
   match e with
-  | p@prp => pure $ (p, ty 0)
-  | t@(ty n) => pure $ (t, (ty $ n + 1))
-  | f@(fall idx e_ty e_body)  => do
-    -- Set type and normal form of bound vars with idx
-    -- to the inferred type of e_ty
-    StateT.modifyGet (⟨(), (idx, ← eval_infer e_ty) :: .⟩)
+    | prp => pure $ ty 0
+    | (ty n) => pure $ ty $ n + 1
+    | (fall idx e_ty e_body)  => do
+      -- Set type and normal form of bound vars with idx
+      -- to the inferred type of e_ty
+      let e_ty' ← infer e_ty
 
-    -- Use new inference rules to infer body type
-    -- This is the type of the entire expression
-    pure (f, (← eval_infer e_body).snd)
-  | a@(abstraction idx e_ty e_body) => do
-    -- Pretty similar thing to forall
-    let norm_e_ty ← eval_infer e_ty
-    StateT.modifyGet (⟨(), (idx, norm_e_ty) :: .⟩)
+      StateT.modifyGet λctx => ⟨(),{ ctx with types := (idx, e_ty') :: ctx.types }⟩
 
-    pure $ (a, fall idx norm_e_ty.snd (← eval_infer e_body).snd)
-  | app (fall idx bind_ty body) rhs => do
-    let (norm_rhs, ty_rhs) ← eval_infer rhs
+      -- Use new inference rules to infer body type
+      -- This is the type of the entire expression
+      infer e_body
+    | (abstraction idx e_ty e_body) => do
+      -- Pretty similar thing to forall
+      let norm_e_ty ← infer e_ty
+      StateT.modifyGet λctx => ⟨(), { ctx with types := (idx, norm_e_ty) :: ctx.types }⟩
 
-    -- These are technically also the same if they are beta
-    -- normally equivalent
-    if ty_rhs != (← eval_infer (bind_ty)).snd then
-      none
+      pure $ fall idx norm_e_ty (← infer e_body)
+    | app lhs rhs => do
+      match ← infer lhs with
+      | (fall idx bind_ty body) =>
+        let ty_rhs ← infer rhs
 
-    StateT.modifyGet (⟨(), (idx, (norm_rhs, ty_rhs)) :: .⟩)
+        -- These are technically also the same if they are beta
+        -- normally equivalent
+        if ty_rhs != bind_ty then
+          none
 
-    eval_infer body
-  | bvar idx => do
-    match (← StateT.get) |> List.filter λ(idx₂, _) => idx == idx₂ with
-    | (_, (v, ty_var))::_ => pure (v, ty_var)
-    | List.nil => none
-  | _ => none
+        pure $ substitute idx body rhs
+      | _ => none
+    | bvar idx => do
+      let { types } ← StateT.get
+      match types |> List.filter λ(idx₂, _) => idx == idx₂ with
+      | (_, ty_var)::_ => pure ty_var
+      | List.nil => none
 
 
-theorem strongly_normalizing
