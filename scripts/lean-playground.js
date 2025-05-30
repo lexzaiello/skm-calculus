73d16f646ddb6dc61eab86d1340d2707ebff8fa3
Dowland Aiello <dowlandaiello@gmail.com>
2025-05-30 11:08:56 -0700

    Revert "Add lean-playground integration?"
    This reverts commit d8b46c57f1025da8eb091cc590e9d97829754748.

--- scripts/lean-playground.js
+++
@@ -1,8 +0,0 @@
-function openLeanPlayground(button) {
-  const codeBlock = button.previousElementSibling.querySelector("code");
-  const code = codeBlock.innerText;
-
-  const playgroundUrl = "https://leanprover.github.io/lean4play/#code=" + encodeURIComponent(code);
-
-  window.open(playgroundUrl, '_blank');
-}
