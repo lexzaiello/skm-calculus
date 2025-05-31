528d10ce0e36a23a534ad4b7a578f708b6d2bde9
Dowland Aiello <dowlandaiello@gmail.com>
2025-05-31 15:17:08 -0700

    Edit mathjaxconfig

--- scripts/mathjaxconfig.js
+++ scripts/mathjaxconfig.js
@@ -1,6 +1,12 @@
-MathJax.Hub.Config({
-  tex2jax: {
-    inlineMath: [ ['$','$'], ["\\(","\\)"] ],
-    processEscapes: true
+window.MathJax = {
+  tex: {
+    inlineMath: [['$', '$'], ['\\(', '\\)']],
+    displayMath: [['$$', '$$'], ['\\[', '\\]']],
+    processEscapes: true,
+    processEnvironments: true
+  },
+  options: {
+    skipHtmlTags: ['script', 'noscript', 'style', 'textarea', 'pre'],
+    ignoreHtmlClass: 'tex2jax_ignore'
   }
-});
+};
