7e1f88b267e807522b6a82b5a6ea3bbfee974e4a
Dowland Aiello <dowlandaiello@gmail.com>
2025-07-18 12:28:24 -0700

    Translation algorithm almost done

--- SkLean/LcSkmEta.lean
+++ SkLean/LcSkmEta.lean
@@ -16,6 +16,22 @@ inductive Expr where
   | m    : Expr
   | ty   : ℕ    → Expr
   | call : Expr → Expr → Expr
+deriving Repr
+
+namespace Expr
+
+def toStringImpl (e : Expr) : String :=
+  match e with
+    | .k          => "K"
+    | .s          => "S"
+    | .m          => "M"
+    | .ty n       => s!"Type {n}"
+    | .call e₁ e₂ => s!"({toStringImpl e₁} {toStringImpl e₂})"
+
+end Expr
+
+instance : ToString Expr where
+  toString := fun e => e.toStringImpl
 
 /-
 For translation purposes, I define an AST for the (optionally) simply-typed \\(\lambda\\)-calculus, extended with our syntax tree. I use de bruijn indices to refer to variables.
@@ -30,6 +46,26 @@ inductive LExpr where
   | s     : LExpr
   | k     : LExpr
   | ty    : ℕ → LExpr
+  | arrow : LExpr → LExpr → LExpr
+deriving Repr
+
+namespace LExpr
+
+def toStringImpl (e : LExpr) : String :=
+  match e with
+    | .var n            => s!"{n}"
+    | .call e₁ e₂       => s!"({e₁.toStringImpl} {e₂.toStringImpl})"
+    | .lam t body       => s!"λ :{t.toStringImpl}.{body.toStringImpl}"
+    | .m                => "M"
+    | .k                => "K"
+    | .s                => "S"
+    | .ty n             => s!"Type {n}"
+    | .arrow t_in t_out => s!"{t_in.toStringImpl} → {t_out.toStringImpl}"
+
+end LExpr
+
+instance : ToString LExpr where
+  toString := fun e => e.toStringImpl
 
 /-
 ## DSL
@@ -95,36 +131,108 @@ Note that in order to represent explicit typing in (nondependent) \\(\lambda\\)-
 
 -- Neither here are point-free
 -- This is what we are trying to generate
-def mk_arrow_lc (a b : LExpr) : LExpr :=
-  (.call (.call (.call .k (.call .m b)) a) b)
-
 def mk_arrow (a b : Expr) : Expr := SKM[(((K (M b)) a) b)]
 
-infix:20 "l~>" => mk_arrow_lc
 infix:20 "~>"  => mk_arrow
 
--- To eliminate λ-abstraction as much as possible
-def lift (e : LExpr) : LExpr :=
+/-
+I make use of a simple, partial `type_of` function which is required to derive the "in-between" and output type in the \\(S\\) transformation, since only the input type is known.
+-/
+
+def type_of (ctx : List LExpr) (e : LExpr) : Option LExpr :=
   match e with
     | (.lam t (.var 0)) =>
-      (.call
-        (.call
-          -- Duplicator S
-          (.call (.call (.call .s (t l~> ((t l~> t) l~> t))) (t l~> (t l~> t))) t)
-          -- First K, takes (x : t) and (K x)
-          (.call (.call .k t) (t l~> t)))
-          -- Second K, takes only one (x : t)
-          (.call (.call k t) t)
-        )
-      )
-      sorry
-    | (.lam t (.call e₁ e₂)) =>
-      sorry
-    | (.lam t (.var n)) =>
-      sorry
-    | (.lam t c) =>
-      sorry
-    | (.call e₁ e₂) =>
-      .call (lift e₁) (lift e₂)
+      some $ .arrow t t
+    | (.lam t (.var n)) => do
+      pure $ (.arrow t (← ctx[n]?))
+    | (.lam t body) => do
+      some (.arrow t $ (← type_of (t :: ctx) body))
+    | .ty n => some $ .ty n.succ
+    | .arrow _ _ => some $ .ty 0
+    | (.call (.call .k α) β) =>
+      pure (.arrow α (.arrow β α))
+    | (.call (.call (.call .s α) β) γ) =>
+      pure ((.arrow (.arrow α (.arrow β γ)) (.arrow (.arrow α β) α)))
+    | (.call .m e) =>
+      type_of ctx e
+    | .k => pure $ .call .m .k
+    | .s => pure $ .call .m .s
+    | .m => pure $ .call .m .m
+    | .call lhs _ =>
+      match type_of ctx lhs with
+        | .some (.arrow _ out_t) =>
+          some out_t
+        | _ => none
+    | .var n =>
+      ctx[n]?
+
+-- So that when we remove a λ abstraction, vars are bound correctly
+def dec_vars (e : LExpr) : LExpr :=
+  match e with
+    | (.var $ n + 1) => .var n
+    | .lam t body =>
+      .lam t $ dec_vars body
+    | .call lhs rhs =>
+      .call (dec_vars lhs) (dec_vars rhs)
+    | .arrow t_in t_out =>
+      .arrow (dec_vars t_in) (dec_vars t_out)
     | x => x
 
+-- To eliminate λ-abstraction as much as possible
+partial def lift (ctx : List LExpr) (e : LExpr) : Option LExpr :=
+  match e with
+    | (.lam t (.var 0)) =>
+      pure (.call
+        (.call
+          -- Duplicator S
+          (.call (.call (.call .s (.arrow t (.arrow (.arrow t t) t))) (.arrow t (.arrow t t))) t)
+          -- First K, takes (x : t) and (K x)
+          (.call (.call .k t) (.arrow t t)))
+          -- Second K, takes only one (x : t)
+        (.call (.call .k t) t))
+    | (.lam t (.call e₁ e₂)) => do
+      let lam_e₁' ← lift ctx (.lam t e₁)
+      let lam_e₂' ← lift ctx (.lam t e₂)
+
+      let t_e₁' ← type_of ctx lam_e₁'
+      let t_e₂' ← type_of ctx lam_e₂'
+
+      pure (LExpr.call
+        -- S duplicator: e₁ takes in duplicated arg, retains its type
+        -- same with e₂
+        -- we combine them
+        (.call (.call (.call (.call .s t_e₁') t_e₂') t)
+        lam_e₁')
+        lam_e₂')
+    | (.lam t c) => do
+      let c' := dec_vars $ (← lift (t :: ctx) c)
+      let t_c ← type_of (t :: ctx) c'
+      pure (.call (.call (.call .k t_c) t) c')
+    | (.call e₁ e₂) => do
+      pure (.call (← lift ctx e₁) (← lift ctx e₂))
+    | x => x
+
+def to_sk (e : LExpr) : Option Expr :=
+  match e with
+    | .ty n  => pure SKM[Ty n]
+    | .call lhs rhs => do
+      pure SKM[(#(← to_sk lhs) #(← to_sk rhs))]
+    | .k => pure SKM[K]
+    | .s => pure SKM[S]
+    | .m => pure SKM[M]
+    | .arrow t_in t_out => do
+      let t_in' ← to_sk t_in
+      let t_out' ← to_sk t_out
+
+      pure $ t_in' ~> t_out'
+    | _ => none
+
+/-
+We can now define the \\(\rightarrow\\) expression using our \\(\lambda\\)-calculus AST and translate to \\(SK(M)\\) using our `lift` and `to_sk` functions. Note that since universes are stratified, \\(->\\) is polymorphic at the meta level to universe levels. In practice, the compiler generates an \\(->\\) expression for all universes used in the program. Furthermore, universes above \\(1\\) are rarely used.
+-/
+
+def arrow_lc (u : ℕ) : LExpr := (.lam (.ty u) (.lam (.ty u) (.call (.call (.call .k (.call .m (.var 0))) (.var 1)) (.var 0))))
+
+def arrow (u : ℕ) := arrow_lc u |> (lift [] .)
+
+#eval arrow 0
