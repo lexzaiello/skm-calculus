795822b513929d5bd90f71d7acb645cc10719662
Dowland Aiello <dowlandaiello@gmail.com>
2025-07-26 15:56:14 -0700

    Work on AST

--- cli/src/Skm/Compiler/Ast.hs
+++ cli/src/Skm/Compiler/Ast.hs
@@ -1,2 +1,68 @@
 module Skm.Compiler.Ast where
 
+import Data.List (intercalate)
+
+-- Calculus of constructions expr, using 0-indexed De Bruijn Indices
+-- Lambda abstractions are optionally typed. Types are inferred
+-- where possible.
+data Expr = Lam (Maybe Expr) Expr
+  | Fall (Maybe Expr) Expr
+  | Var Int
+  | App Expr Expr
+  | S
+  | K
+  | M
+
+-- Human readable, not used anywhere except for serialization purposes
+data ReadableExpr = HLam String (Maybe ReadableExpr) ReadableExpr
+  | HFall String (Maybe ReadableExpr) ReadableExpr
+  | HVar String
+  | HApp ReadableExpr ReadableExpr
+  | Hs
+  | Hk
+  | Hm
+
+unwordtokens :: [[Token]] -> [Token]
+unwordtokens = intercalate Space
+
+tokenize :: ReadableExpr -> [[Token]]
+tokenize Hs = pure Ts
+tokenize Hk = pure Tk
+tokenize Hm = pure Tm
+tokenize HLam  binder (Some bind_ty) body = [[Lambda], [LParen, (Ident binder)], [Colon], (tokenize bind_ty ++ [RParen]), [FatArrow], tokenize body]
+tokenize HLam  binder Nothing        body = [[Lambda], (Ident binder), [FatArrow], tokenize body]
+tokenize HFall binder (Some bind_ty) body = [[Fall], [LParen, (Ident binder)], [Colon], (tokenize bind_ty ++ [RParen, Comma]), tokenize body]
+tokenize HFall binder Nothing        body = [[Fall], [(Ident binder), Comma], tokenize body]
+tokenize HVar v       = [Ident v]
+tokenize HApp lhs rhs = [[LParen] ++ tokenize lhs, tokenize rhs ++ [RParen]]
+
+data Token = LParen
+  | RParen
+  | Lambda
+  | Space
+  | FatArrow
+  | Fall
+  | Comma
+  | Def
+  | Colon
+  | Eq
+  | Ident String
+  | Ts
+  | Tk
+  | Tm
+
+instance Show Token where
+  show LParen   = "("
+  show RParen   = ")"
+  show Space    = " "
+  show FatArrow = "=>"
+  show Def      = "def"
+  show Colon    = ":"
+  show Eq       = "="
+  show Ident i  = i
+  show Lambda   = "λ"
+  show Ts       = "S"
+  show Tk       = "K"
+  show Tm       = "M"
+  show Fall     = "∀"
+  show Comma    = ","
