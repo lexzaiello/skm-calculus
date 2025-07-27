a83cb73a651a517b9b77a515254ae2a95a3d2bca
Dowland Aiello <dowlandaiello@gmail.com>
2025-07-27 16:18:25 -0700

    Bootstrapping types of core combinators from compiler

--- cli/src/Skm/Eval.hs
+++ cli/src/Skm/Eval.hs
@@ -1,38 +1,47 @@
 module Skm.Eval where
 
+import Control.Applicative
 import Skm.Ast
-import Skm.Std
 import Data.Maybe (fromMaybe)
 
-step :: Expr -> Maybe Expr
-step (Call (Call K x) _y) = Just x
-step (Call (Call (Call S x) y) z) = Just (Call (Call x z) (Call y z))
-step (Call M K) = Just t_k
-step (Call M S) = Just t_s
-step (Call M M) = Just t_m
-step (Call M (Call lhs rhs)) = Just (Call t_out (Call (Call M lhs) rhs))
+data EvalConfig = EvalConfig
+  { tIn   :: Expr
+  , tOut  :: Expr
+  , tK    :: Expr
+  , tS    :: Expr
+  , tM    :: Expr
+  , arrow :: Expr
+  }
+
+step :: EvalConfig -> Expr -> Maybe Expr
+step _ (Call (Call K x) _y)              = Just x
+step _ (Call (Call (Call S x) y) z)      = Just (Call (Call x z) (Call y z))
+step (EvalConfig { tK = tK }) (Call M K) = Just t_k
+step (EvalConfig { tS = tS }) (Call M S) = Just t_s
+step (EvalConfig { tM = tM }) (Call M M) = Just t_m
+step (Call M (Call lhs rhs))             = Just (Call (Call (Call M lhs) rhs) t_out)
 step _ = Nothing
 
 -- Attempts to reduce the next available outermost redex
-eval_n :: Int -> Expr -> Expr
+eval_n :: EvalConfig -> Int -> Expr -> Expr
 eval_n n e
   | n <= 0 = e
-  | otherwise = case e of
-    (Call lhs rhs) ->
-      let call' = (Call (eval_n (n - 1) lhs) (eval_n (n - 1) rhs))
-          e'    = fromMaybe call' $ step call' in
-        if e' == e then
-          e
-        else
-          eval_n (n - 2) e'
-    x -> x
+  | otherwise =
+    case e of
+      (Call lhs rhs) ->
+        let call' = (Call (eval_n (n - 1) lhs) rhs)
+            e'    = fromMaybe call' $ step call' in
+          if e' == e then
+            e
+          else
+            eval_n (n - 2) e'
+      x -> x
 
-eval :: Expr -> Expr
+eval :: EvalConfig -> Expr -> Expr
 eval e =
   case e of
     (Call lhs rhs) ->
-      let call' = (Call (eval lhs) rhs)
-          e'    = fromMaybe call' $ step (Call lhs rhs) in
+      let e' = fromMaybe (Call (eval lhs) rhs) $ step e in
         if e' == e then
           e
         else
