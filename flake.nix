761c8855595ce8f37431e678b25c41e381e1818f
Dowland Aiello <dowlandaiello@gmail.com>
2025-07-30 10:40:08 -0700

    Update cabal, add cabal to flake

--- flake.nix
+++ flake.nix
@@ -98,7 +98,7 @@
           program = "${serve-bin}/bin/serve";
         };
         devShells.default = with pkgs.haskellPackages; pkgs.mkShell {
-          nativeBuildInputs = [ ghc hpack haskell-language-server ];
+          nativeBuildInputs = [ ghc hpack haskell-language-server cabal-install ];
         };
         apps.skm = {
           type = "app";
