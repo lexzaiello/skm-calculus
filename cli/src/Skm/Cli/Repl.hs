926a048fea85e0a2300338436a73dd3d3ad82c63
Dowland Aiello <dowlandaiello@gmail.com>
2025-08-01 12:19:07 -0700

    Repl v2

--- cli/src/Skm/Cli/Repl.hs
+++ cli/src/Skm/Cli/Repl.hs
@@ -1,15 +1,21 @@
 module Skm.Cli.Repl where
 
+import Data.Text (pack)
+import Control.Monad.Trans.Class
+import Text.Printf (printf)
 import Control.Monad.IO.Class (liftIO)
 import Control.Monad.Trans.Except
-import Skm.Eval (EvalConfig)
+import Skm.Eval (EvalConfig(..))
 import Skm.Vm (eval)
-import Skm (Error, ccResultToGenResult, execResultToGenResult)
-import Skm.Compiler.Ast (CompilationError, parseResultToCompilationResult)
+import Skm (Error(..), ccResultToGenResult, execResultToGenResult)
+import Skm.Compiler.Ast (CompilationError(..), parseResultToCompilationResult)
 import System.IO (stdout, hFlush)
 import Data.Text (pack)
 import Skm.Cli.Exec
 import Skm.Cli.OptParse (EvalMode(..))
+import System.Console.Haskeline
+
+type RawExpr = String
 
 promptPs :: String
 promptPs = ">> "
@@ -32,3 +38,27 @@ repl eCfg mode = do
   liftIO $ print e'
 
   repl eCfg mode
+
+exprSession :: RawExpr -> EvalConfig -> EvalMode -> InputT (ExceptT Error IO) ()
+exprSession ctxExpr cfg eMode = do
+  minput <- getInputLine $ printf "%s %s" ctxExpr promptPs
+  case minput of
+    Just "exit" -> return ()
+    Just "parse" ->
+      (lift . ExceptT . pure . liftErr) (case eMode of
+        Raw -> fmap show $ parseSk streamStdinName $ pack ctxExpr
+        Lc -> fmap show $ parseExprCoc streamStdinName $ pack ctxExpr) >>= outputStrLn
+    Nothing -> return ()
+  where liftErr  = either (Left . CompError . ParseFailure) Right
+
+root :: EvalConfig -> EvalMode -> InputT (ExceptT Error IO) ()
+root cfg mode = do
+  minput <- getInputLine $ printf promptPs
+  case minput of
+    Just "exit" -> return ()
+    Just input ->
+      exprSession input cfg mode
+    Nothing -> return ()
+
+repl' :: EvalConfig -> EvalMode -> ExceptT Error IO ()
+repl' cfg mode = liftIO $ runInputT defaultSettings (root cfg mode)
