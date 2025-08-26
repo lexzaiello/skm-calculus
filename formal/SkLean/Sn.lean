6fb5498079ca8ae112ca944986928f31d1da744f
Dowland Aiello <dowlandaiello@gmail.com>
2025-08-26 10:11:55 -0700

    Reducibility candidates, use Acc instead of hand-rolled sn

--- formal/SkLean/Sn.lean
+++ formal/SkLean/Sn.lean
@@ -1,13 +1,12 @@
 import SkLean.Ast3
 
-inductive sn : Expr → Prop
-  | hard : (∀ e', is_eval_once e e' → sn e') → sn e
+def sn (e : Expr) : Prop := Acc (λ e' e => is_eval_once e e') e
 
 syntax (name := do_stuck) "do_stuck " : tactic
 
 macro_rules
   | `(tactic| do_stuck) =>
-    `(tactic| apply sn.hard; intro e h_step; contradiction)
+    `(tactic| apply Acc.intro; intro e h_step; contradiction)
 
 namespace sn
 
@@ -26,8 +25,11 @@ lemma m : sn SKM[M] := by
 lemma preserved : sn e → is_eval_once e e' → sn e' := by
   intro h_sn h_eval
   induction h_sn
-  case hard e ih₁ ih₂ =>
-    simp_all
+  case intro x ih₁ ih₂ =>
+    exact ih₁ _ h_eval
+
+lemma intro (h : ∀ e', is_eval_once e e' → sn e') : sn e :=
+  Acc.intro e h
 
 end sn
 
@@ -36,36 +38,24 @@ lemma is_value_sn (h_v : is_value e) : sn e := by
   repeat do_stuck
 
 inductive is_candidate_for_ty : Expr → Expr → Prop
-  | val   : valid_judgment e t
+  | val : valid_judgment e t
     → is_value e
     → is_candidate_for_ty e t
-  | step  : valid_judgment e t
-    → is_candidate_for_ty e t
-    → is_eval_once e e'
-    → is_candidate_for_ty e' t
-  | stuck : valid_judgment e t
-    → (∀ e', is_eval_once e e' → is_candidate_for_ty e' t)
-    → is_candidate_for_ty e t
+  | app : is_candidate_for_ty lhs SKM[(t_in ~> t_out)]
+    → is_candidate_for_ty rhs t_out
+    → (∀ e', is_eval_once SKM[(lhs rhs)] e' → is_candidate_for_ty e' t_out)
+    → is_candidate_for_ty SKM[(lhs rhs)] t_out
 
 namespace is_candidate_for_ty
 
 lemma all_sn (h_candidate : is_candidate_for_ty e t) : sn e := by
   induction h_candidate
   exact is_value_sn (by assumption)
-  exact sn.preserved (by assumption) (by assumption)
-  case stuck e' t' h_t_e ih₁ ih₂ =>
-    have h_t'' := h_t_e.progress
-    match h_t'' with
-      | .inl h_stuck =>
-        exact is_value_sn (by assumption)
-      | .inr ⟨t, h_step⟩ =>
-        apply sn.hard
-        simp_all
+  apply sn.intro
+  assumption
 
 lemma all_well_typed (h_candidate : is_candidate_for_ty e t) : valid_judgment_hard e t := by
   induction h_candidate
-  assumption
-  apply valid_judgment.preservation
   
 
 end is_candidate_for_ty
