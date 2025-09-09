993ca091a51c08a3424c98e122c0f43b18d89e20
Dowland Aiello <dowlandaiello@gmail.com>
2025-09-08 18:12:20 -0700

    DSL cleanup

--- formal/SkLean/Ast3.lean
+++ formal/SkLean/Ast3.lean
@@ -32,14 +32,11 @@ inductive ExprBox (e : Ast.Expr) where
 
 declare_syntax_cat skmexpr
 syntax "@" skmexpr                                     : skmexpr
-syntax "K" skmexpr skmexpr                             : skmexpr
-syntax "S" skmexpr skmexpr skmexpr                     : skmexpr
-syntax "K" skmexpr skmexpr                             : skmexpr
-syntax "S" skmexpr skmexpr skmexpr                     : skmexpr
 syntax "K₀"                                            : skmexpr
-syntax "K+" skmexpr skmexpr skmexpr*                   : skmexpr
-syntax "(" "_" ":" skmexpr ")"                         : skmexpr
-syntax "(" skmexpr ":" skmexpr ")"                     : skmexpr
+syntax "K+"                                            : skmexpr
+syntax "K"                                             : skmexpr
+syntax "S"                                             : skmexpr
+syntax skmexpr ":" skmexpr                             : skmexpr
 syntax "λ" skmexpr skmexpr* "=>" skmexpr               : skmexpr
 syntax "I" skmexpr                                     : skmexpr
 syntax "S₀"                                            : skmexpr
@@ -48,7 +45,6 @@ syntax "~>"                                            : skmexpr
 syntax "<~"                                            : skmexpr
 syntax "→"                                             : skmexpr
 syntax "←"                                             : skmexpr
-syntax "always" skmexpr                                : skmexpr
 syntax "Ty" term                                       : skmexpr
 syntax "Prp"                                           : skmexpr
 syntax "self" skmexpr                                  : skmexpr
@@ -59,7 +55,6 @@ syntax skmexpr "→" skmexpr                             : skmexpr
 syntax skmexpr "←" skmexpr                             : skmexpr
 syntax "codomain" skmexpr                              : skmexpr
 syntax "domain" skmexpr                                : skmexpr
-syntax "(" skmexpr skmexpr ")"                         : skmexpr
 syntax "(" skmexpr skmexpr skmexpr* ")"                : skmexpr
 syntax ident                                           : skmexpr
 syntax "#" term                                        : skmexpr
@@ -74,29 +69,25 @@ macro_rules
   | `(SKM[ $e:skmexpr ])  => `(⟪ $e ⟫)
 
 macro_rules
-  | `(⟪ domain $t_in ~> $_t_out ⟫) => pure t_in
-  | `(⟪ codomain $_t_in ~> $t_out ⟫) => pure t_out
-  | `(⟪ ((@(→) $t₁ $t₂) : ?) ⟫) => `(SKM[(∀ (_ : $t₁) (_ : $t₂), Ty (max (max_universe m) (max_universe n)).succ)])
-  | `(⟪ (K α β : ?) ⟫) => `(SKM[(K α β : ∀ (_ : α) (_ : β), α)])
-  | `(⟪ (S ? ? ? ($x : $t_x)) ⟫) => `(SKM[(S (codomain (codomain $t_x)) (domain (codomain $t_x)) (domain $t_x) $x)])
-  | `(⟪ ($e₁ $e₂ $rest*) ⟫) => match rest.toList with
-    | x :: xs => `(SKM[(($e₁ $e₂) $x $(⟨xs⟩)*)])
-    | _ => `(SKM[($e₁ $e₂)])
-  | `(⟪ (_ : $t:skmexpr )⟫) => `(SKM[$t])
-  | `(⟪ α ~> always $t:skmexpr ⟫)          => `(SKM[α ~> (K ? α $t)])
-  | `(⟪ @K #$m #$n ⟫) => `(Expr.k $m $n)
-  | `(⟪ @S #$m #$n #$o ⟫) => `(Expr.s $m $n $o)
-  | `(⟪ K $m:skmexpr $n:skmexpr ⟫)       => `(SKM[(((@K #(max_universe ⟪$m⟫) #(max_universe ⟪$n⟫)) $m) $n)])
+  | `(⟪ (M ($_e:skmexpr : $t:skmexpr)) ⟫) => pure t
+  | `(⟪ domain $t_in:skmexpr ~> $_t_out:skmexpr ⟫) => pure t_in
+  | `(⟪ codomain $_t_in:skmexpr ~> $t_out:skmexpr ⟫) => pure t_out
+  | `(⟪ ((@(→) $t₁:skmexpr $t₂:skmexpr) : ?) ⟫) => `(SKM[(∀ (x : $t₁) (y : $t₂), Ty (max (max_universe m) (max_universe n)).succ)])
+  | `(⟪ (K α β) : ? ⟫) => `(SKM[(K α β) : ∀ (x : α) (y : β), α])
+  | `(⟪ (S ? ? ? ($x:skmexpr : $t_x:skmexpr)) ⟫) => `(SKM[(S (codomain (codomain $t_x)) (domain (codomain $t_x)) (domain $t_x) $x)])
+  | `(⟪ (@S #$m:term #$n:term #$o:term) ⟫) => `(Expr.s $m $n $o)
+  | `(⟪ (@K #$m:term #$n:term) ⟫) => `(Expr.k $m $n)
+  | `(⟪ (S $m:skmexpr $n:skmexpr $o:skmexpr) ⟫) => `(SKM[((((@S #(max_universe ⟪$m⟫) #(max_universe ⟪$n⟫) #(max_universe ⟪$o⟫)) $m) $n) $o)])
   | `(⟪ (K ? $t_y:skmexpr ($e : $et)) ⟫) => `(SKM[((K $et $t_y) $e)])
+  | `(⟪ (K $m:skmexpr $n:skmexpr) ⟫)       => `(SKM[(((@K #(max_universe ⟪$m⟫) #(max_universe ⟪$n⟫)) $m) $n)])
   | `(⟪ (K ? $t_y:skmexpr $e) ⟫)         => `(SKM[((K (M $e) $t_y) $e)])
-  | `(⟪ K+ $t:skmexpr $x:skmexpr $tys:skmexpr* ⟫) =>
+  | `(⟪ ((K+ ? $x:skmexpr $tys:skmexpr*) $e:skmexpr) ⟫) => `(SKM[((K+ (M $e) $x $tys*) $e)])
+  | `(⟪ (K+ $t:skmexpr $x:skmexpr $tys:skmexpr*) ⟫) =>
     match tys.toList with
     | next :: xs =>
       let tys' := Array.mk xs.reverse
       `(SKM[((K+ (M (K $t $x)) $next $tys'*) (K $t $x))])
     | _ => `(SKM[(K $t $x)])
-  | `(⟪ (K+ ? $x:skmexpr $tys:skmexpr* $e:skmexpr) ⟫) => `(SKM[((K+ (M $e) $x $tys*) $e)])
-  | `(⟪ S $m:skmexpr $n:skmexpr $o:skmexpr ⟫) => `(SKM[((((@S #(max_universe ⟪$m⟫) #(max_universe ⟪$n⟫) #(max_universe ⟪$o⟫)) $m) $n) $o)])
   | `(⟪ M ⟫)                            => `(Expr.m)
   | `(⟪ ? ⟫)                            => `(Expr.hole)
   | `(⟪ Ty $n:term ⟫)                   => `(Expr.ty $n)
@@ -107,21 +98,29 @@ macro_rules
   | `(⟪ ← ⟫)                            => `(Expr.imp')
   | `(⟪ $e₁:skmexpr ~> $e₂:skmexpr ⟫)   => `(SKM[(((~>) $e₁) $e₂)])
   | `(⟪ $e₁:skmexpr <~ $e₂:skmexpr ⟫)   => `(SKM[($e₂ ~> $e₁)])
-  | `(⟪ $e₁:skmexpr → $e₂:skmexpr ⟫)    => `(SKM[∀ (_ : $e₁), $e₂])
+  | `(⟪ $e₁:skmexpr → $e₂:skmexpr ⟫)    => `(SKM[∀ (x : $e₁), $e₂])
   | `(⟪ $e₁:skmexpr ← $e₂:skmexpr ⟫)    => `(SKM[$e₂ → $e₁])
   | `(⟪ $e:ident ⟫)                     => `($e)
   | `(⟪ # $e:term ⟫)                    => `($e)
   | `(⟪ ($e:skmexpr) ⟫)                 => `(⟪$e⟫)
-  | `(⟪ ($e₁:skmexpr $e₂:skmexpr) ⟫)    => `(Expr.call ⟪ $e₁ ⟫ ⟪ $e₂ ⟫)
   -- Accepts an expression of type e, returning type e
   | `(⟪ self $e:skmexpr ⟫)              => `(SKM[(K (M $e) $e $e)])
-  | `(⟪ λ (_ : $t:skmexpr) $tys:skmexpr* => $body:skmexpr ⟫) => `(SKM[((K+ (M $body) $t $tys*) $body)])
-  | `(⟪ ∀ (x : $t:skmexpr), x ⟫) => `(SKM[(~> self $t)])
-  | `(⟪ ∀ (x : $t:skmexpr), (f x) ⟫) => `(SKM[self $t ~> f])
-  | `(⟪ ∀ (_ : $t:skmexpr) $tys:skmexpr*, $body:skmexpr ⟫) => do
+  | `(⟪ λ (_v : $t:skmexpr) $tys:skmexpr* => $body:skmexpr ⟫) => do
     let tys := [t] ++ tys.toList.filterMap (λ stx =>
       match stx with
-      | `(skmexpr| (_ : $t)) => pure t
+      | `(skmexpr| (_v : $t)) => pure t
+      | _ => none)
+
+    `(SKM[((K+ (M $body) $t $(⟨tys⟩)*) $body)])
+  | `(⟪ ($e₁:skmexpr $e₂:skmexpr $rest:skmexpr*) ⟫) => match rest.toList with
+    | x :: xs => `(SKM[(($e₁ $e₂) $x $(⟨xs⟩)*)])
+    | _ => `(Expr.call ⟪$e₁⟫ ⟪$e₂⟫)
+  | `(⟪ ∀ ($x:skmexpr : $t:skmexpr), ($f:skmexpr $x) ⟫) => `(SKM[self $t ~> f])
+  | `(⟪ ∀ ($x:skmexpr : $t:skmexpr), $x ⟫) =>`(SKM[(~> self $t)])
+  | `(⟪ ∀ ($_:skmexpr : $t:skmexpr) $tys:skmexpr*, $body:skmexpr ⟫) => do
+    let tys := [t] ++ tys.toList.filterMap (λ stx =>
+      match stx with
+      | `(skmexpr| ($_:skmexpr : $t:skmexpr)) => pure t
       | _ => none)
 
     let e_body ← (`(skmexpr| ((K+ (M $body) $t $(⟨tys⟩)*) $body)))
@@ -130,12 +129,15 @@ macro_rules
       | t :: xs => pure (← (`(skmexpr| ((λ (_:$t) $(⟨← xs.mapM (λ e => `(skmexpr| (_:$e)))⟩)* => $t_out) ~> $e))), xs)
       | _ => pure (← (`(skmexpr| K+ ? $t $(⟨tys⟩)* body)), [])) (e_body, tys))).fst)⟫)
 
+#eval SKM[(K (Ty 0) (Ty 2))]
+#eval SKM[(λ (_v : Ty 0) (_x : Ty 2) => Ty 0)]
+
 namespace Expr
 
 def toStringImpl (e : Expr) : String :=
   match e with
-  | SKM[@S #_m #n #o]  => s!"S.{_m},{n},{o}"
-  | SKM[@K #_m #n]    => s!"K.{_m},{n}"
+  | SKM[(@S #_m #n #o)]  => s!"S.{_m},{n},{o}"
+  | SKM[(@K #_m #n)]    => s!"K.{_m},{n}"
   | SKM[M]    => "M"
   | SKM[Ty n] => s!"Type {n}"
   | SKM[Prp]  => "Prop"
@@ -164,25 +166,6 @@ def pop_arrow : Ast.Expr → Ast.Expr
   | e => e
 
 end Expr
-
-def mk_k_type_eta (α β : Expr) : Expr :=
-  let inner_k := SKM[((K ? (M β)) α)]
-
-  SKM[(α ~> (β ~> ((K ? α) inner_k)))]
-
-syntax "I" skmexpr : skmexpr
-
-def i (t : Expr) : Expr :=
-  let t_k_right := mk_k_type_eta t t
-
-  let k_right := SKM[(K t t)]
-  let k_left := SKM[(K t t_k_right)]
-
-  SKM[(((S t (t → t) t) k_left) k_right)]
-
-macro_rules
-  | `(⟪ I $t:skmexpr ⟫) => `(SKM[#(i ⟪$t⟫)])
-
 
 def mk_k_type (_m n : Universe) : Ast.Expr :=
   SKM[Ty _m ~> Ty n ~> (((((K _m n) Ty _m) Ty n) (~>)) (<~))]
