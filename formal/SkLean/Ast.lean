f4528bb929d79a93d09b940d52626e2ea9a062dc
Dowland Aiello <dowlandaiello@gmail.com>
2025-10-04 14:33:01 -0700

    Elaborating on interpretation of S and K

--- formal/SkLean/Ast.lean
+++ formal/SkLean/Ast.lean
@@ -1,7 +1,11 @@
 /-
 # Ast
 
-SK \\(\Pi\\) achieves dependent typing by introducing a \\(\Pi\\) combinator. The evaluation rules for \\(\Pi\\) are quite simple, and its interpretation as a dependent type formation rule are quite natural:
+SK \\(\Pi\\) achieves dependent typing by introducing a \\(\Pi\\) combinator.
+
+## The \\(\Pi\\) Combinator
+
+The evaluation rules for \\(\Pi\\) are quite simple, and its interpretation as a dependent type formation rule are quite natural:
 
 $$
 (\Pi\ \alpha\ c)\ \text{arg} \to c\ \text{arg} \\\\
@@ -21,6 +25,37 @@ $$
 &\to \text{Type}
 \end{align}
 $$
+
+By the evaluation rules for \\(\Pi\\), non-dependent types may be formed with \\(K\\), which discards the relevant argument, while \\(S\\) or \\(I\\) embed the argument within the body of the \\(\Pi\\)-type. For example, the expression \\(\Pi (x : \alpha), x\\) may be formed by the SK \\(\Pi\\) expression \\(\Pi\ \alpha\ I\\), where \\(I\\) is the identity combinator.
+
+$$
+\Pi (x : \alpha), x \cong \Pi\ \alpha\ I
+$$
+
+Note that, so far, I have been referring to the untyped varieties of \\(S\\), \\(K\\), and \\(I\\).
+
+## SK Definitions
+
+I follow the canonical evaluation rules for \\(K\\) and \\(S\\). However, I extend these rules to permit explicit type arguments to \\(K\\) and \\(S\\):
+
+$$
+K \alpha \beta x y\to x \\\\
+S \alpha \beta \gamma x y z \to (x z) (y z)
+$$
+
+I interpret \\(K\\) and \\(S\\) in their dependently-typed forms as is typically done in the literature:
+
+$$
+K : \Pi (\alpha : \text{Type}) (\beta : \text{Type}) (x : \alpha) (y : \alpha), \alpha
+$$
+
+The dependent type of \\(S\\) is slightly more complicated:
+
+$$
+S : \Pi (\alpha : \text{Type}) (\beta : \alpha \rightarrow \text{Type}) (\gamma : \alpha \rightarrow \text{Type}) \\\\ (x : \Pi (x : \alpha), \beta x \rightarrow \gamma x) (y : \Pi (x : \alpha), \beta x) (z : \alpha), \gamma x
+$$
+
+
 -/
 
 example : 1 = 1 := rfl
