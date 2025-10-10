92cb88fe49c5d45c59475d25ed9860df7ea66a13
Dowland Aiello <dowlandaiello@gmail.com>
2025-10-10 12:16:58 -0700

    More nice macros for latex webbook

--- scripts/mathjaxconfig.js
+++ scripts/mathjaxconfig.js
@@ -1,17 +1,19 @@
 window.MathJax = {
-  loader: {load: ['[tex]/mathtools']},
+  loader: {load: ["[tex]/mathtools"]},
   tex: {
-    inlineMath: [['$', '$'], ['\\(', '\\)']],
-    displayMath: [['$$', '$$'], ['\\[', '\\]']],
-    packages: ['base', 'ams', 'mathtools'],
+    inlineMath: [["$", "$"], ["\\(", "\\)"]],
+    displayMath: [["$$", "$$"], ["\\[", "\\]"]],
+    packages: ["base", "ams", "mathtools"],
     processEscapes: true,
     processEnvironments: true
     macros: {
-      type: ["{{\\text{Type}\ #1}}", 1]
+      type: ["{{\\text{Type}\ #1}}", 1],
+      pie: ["{{\\Pi\\ (#1)\\ (#2)}}", 2],
+      turnstile: ["{{\\Gamma \\vdash #1 : #2}}", 2]
     }
   },
   options: {
-    skipHtmlTags: ['script', 'noscript', 'style', 'textarea', 'pre'],
-    ignoreHtmlClass: 'tex2jax_ignore'
+    skipHtmlTags: ["script", "noscript", "style", "textarea", "pre"],
+    ignoreHtmlClass: "tex2jax_ignore"
   }
 };
